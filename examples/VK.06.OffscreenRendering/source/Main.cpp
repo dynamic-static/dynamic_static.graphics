9bec4fa35c2c2328c42d24df3867c563ee857568
Patrick Purcell <p.84@live.com>
2017-08-22 11:52:22 -0700

    Refactoring RenderPass creation

--- examples/VK.06.OffscreenRendering/source/Main.cpp
+++ examples/VK.06.OffscreenRendering/source/Main.cpp
@@ -637,55 +637,6 @@ int main()
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         // Create RenderPass
-        // VkAttachmentDescription colorAttachment { };
-        // colorAttachment.format = swapchain->format();
-        // colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-        // colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-        // colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-        // colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-        // colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        // colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-        // colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-        // VkAttachmentReference colorAttachmentReference { };
-        // colorAttachmentReference.attachment = 0;
-        // colorAttachmentReference.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-        // 
-        // std::array<VkFormat, 3> depthFormats {
-        //     VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT
-        // };
-        // 
-        // auto depthFormat = physicalDevice.find_supported_format(
-        //     depthFormats,
-        //     VK_IMAGE_TILING_OPTIMAL,
-        //     VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT
-        // );
-        // 
-        // VkAttachmentDescription depthAttachment { };
-        // depthAttachment.format = depthFormat;
-        // depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
-        // depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-        // depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        // depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-        // depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        // depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-        // depthAttachment.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-        // VkAttachmentReference depthAttachmentReference { };
-        // depthAttachmentReference.attachment = 1;
-        // depthAttachmentReference.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
-        // 
-        // VkSubpassDescription subpass { };
-        // subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-        // subpass.colorAttachmentCount = 1;
-        // subpass.pColorAttachments = &colorAttachmentReference;
-        // subpass.pDepthStencilAttachment = &depthAttachmentReference;
-        // 
-        // std::array<VkAttachmentDescription, 2> attachments { colorAttachment, depthAttachment };
-        // RenderPass::Info renderPassInfo;
-        // renderPassInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
-        // renderPassInfo.pAttachments = attachments.data();
-        // renderPassInfo.subpassCount = 1;
-        // renderPassInfo.pSubpasses = &subpass;
-        // auto renderPass = device->create<RenderPass>(renderPassInfo);
         VkFormat depthFormat = VK_FORMAT_UNDEFINED;
         auto renderPass = create_non_reflective_render_pass(*device, swapchain->format(), depthFormat);
 
