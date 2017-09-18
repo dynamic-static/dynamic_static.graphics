42d6e29eb9f349b74132c18321db6096fa766ba4
Patrick Purcell <p.84@live.com>
2017-09-17 19:24:09 -0700

    General cleanup

--- examples/VK.06.OffscreenRendering/source/Main.cpp
+++ examples/VK.06.OffscreenRendering/source/Main.cpp
@@ -1,7 +1,7 @@
 
 /*
 ==========================================
-    Copyright (c) 2017 Dynamic_Static 
+    Copyright (c) 2017 Dynamic_Static
     Licensed under the MIT license
     http://opensource.org/licenses/MIT
 ==========================================
