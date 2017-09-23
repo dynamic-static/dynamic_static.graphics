1634871f1430151fccf2f6199d96f34eea127ed7
Patrick Purcell <p.84@live.com>
2017-09-22 18:12:50 -0700

    Adding VertexPositionTexCoord

---
+++ include/Dynamic_Static/Graphics/Vulkan/VertexTypes/VertexPositionTexCoord.hpp
@@ -0,0 +1,42 @@
+
+/*
+==========================================
+    Copyright (c) 2017 Dynamic_Static
+    Licensed under the MIT license
+    http://opensource.org/licenses/MIT
+==========================================
+*/
+
+#pragma once
+
+#include "Dynamic_Static/Core/Math.hpp"
+#include "Dynamic_Static/Graphics/Vulkan/Defines.hpp"
+#include "Dynamic_Static/Graphics/Vulkan/Vertex.hpp"
+
+#include <array>
+
+namespace Dynamic_Static {
+namespace Graphics {
+namespace Vulkan {
+
+    /**
+     * TODO : Documentation.
+     */
+    struct VertexPositionTexCoord final
+    {
+        dst::Vector3 position;
+        dst::Vector2 texCoord;
+    };
+
+    /**
+     * TODO : Documentation.
+     */
+    template <>
+    inline auto attribute_descriptions<VertexPositionTexCoord>(uint32_t binding)
+    {
+        return create_attribute_descriptions<dst::Vector3, dst::Vector2>(binding);
+    }
+
+} // namespace Vulkan
+} // namespace Graphics
+} // namespace Dynamic_Static
