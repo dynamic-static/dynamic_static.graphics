6d01b90b5df43beb0f6a6df5a9bd6e468a344046
Patrick Purcell <p.84@live.com>
2017-11-05 23:40:32 -0800

    Adhoc fix for dst.core transitive link

--- external/Dynamic_Static.Core.cmake
+++ external/Dynamic_Static.Core.cmake
@@ -12,8 +12,20 @@ ExternalProject_Add(
     INSTALL_COMMAND ""
 )
 
+# TODO : Why doesn't transitive linking work when we let CMake auto link and include Dynamic_Static.Core?
+#        It's OK to do it explicitly for now, but this will need to be addressed sooner or later.
+# NOTE : This comment is duplicated in Dynamic_Static.Graphics/source/CMakeLists.txt
+# ExternalProject_Get_Property(Dynamic_Static.Core.package BINARY_DIR)
+# set(Dynamic_Static.Core.PACKAGE ${BINARY_DIR}/Dynamic_Static.Core.package.cmake)
+# if (EXISTS ${Dynamic_Static.Core.PACKAGE})
+#     include(${Dynamic_Static.Core.PACKAGE})
+# endif()
+
+ExternalProject_Get_Property(Dynamic_Static.Core.package SOURCE_DIR)
 ExternalProject_Get_Property(Dynamic_Static.Core.package BINARY_DIR)
-set(Dynamic_Static.Core.PACKAGE ${BINARY_DIR}/Dynamic_Static.Core.package.cmake)
-if (EXISTS ${Dynamic_Static.Core.PACKAGE})
-    include(${Dynamic_Static.Core.PACKAGE})
+set(Dynamic_Static.Core.INCLUDE "${SOURCE_DIR}/include/")
+if (MSVC)
+    set(Dynamic_Static.Core.LIBRARY "${BINARY_DIR}/source/$(Configuration)/Dynamic_Static.Core.lib")
+else()
+    set(Dynamic_Static.Core.LIBRARY "${BINARY_DIR}/source/Dynamic_Static.Core.a")
 endif()
