c414370564911ad5c9c77ffb04a6b4390e2f7659
Patrick Purcell <patrick.purcell@intel.com>
2020-04-27 21:28:40 -0700

    Setting up create structure copy

--- generator/source/create-structure-copy-generator.hpp
+++ generator/source/create-structure-copy-generator.hpp
@@ -13,10 +13,19 @@
 #include "dynamic_static/vk-xml-parser.hpp"
 
 #include <fstream>
+#include <set>
 
 namespace dst {
 namespace vk {
 namespace cppgen {
+
+inline bool structure_requires_custom_handling(const dst::cppgen::CppFunction& cppFunction)
+{
+    static const std::set<std::string> sStructuresRequiringCustomHandling {
+        "VkTransformMatrixKHR"
+    };
+    return sStructuresRequiringCustomHandling.count(string::remove(string::remove(cppFunction.cppParameters[0].type, "const "), "&"));
+}
 
 inline std::string strip_vk(const std::string& str)
 {
@@ -42,36 +51,19 @@ public:
                 continue;
             }
             CppFunction cppFunction { };
-            cppFunction.cppDocumentation.brief = "Gets a deep copy of a given " + structure.name;
-            cppFunction.cppDocumentation.notes.push_back(
-                string::replace(
-                    "Any ${VK_STRUCTURE_TYPE} copied via create_structure_copys() must be destroyed via destroy_structure_copy()",
-                    {{ "${VK_STRUCTURE_TYPE}", structure.name }}
-                )
-            );
-            cppFunction.cppDocumentation.notes.push_back(
-                string::replace(
-                    "Prefer Managed<${VK_STRUCTURE_TYPE}> over direct usage of create_structure_copy()/destroy_structure_copy()",
-                    {{ "${VK_STRUCTURE_TYPE}", structure.name }}
-                )
-            );
             cppFunction.cppCompileGuards.insert(structure.compileGuard);
             cppFunction.cppTemplate.cppSpecializations.push_back(structure.name);
             cppFunction.cppReturn = structure.name;
             cppFunction.name = "create_structure_copy";
-            CppParameter vkStructureCppParameter { };
-            vkStructureCppParameter.cppDocumentation.brief = "A const reference to the ${VK_STRUCTURE_TYPE} to copy";
-            vkStructureCppParameter.type = "const " + structure.name + "&";
-            vkStructureCppParameter.name = "obj";
-            cppFunction.cppParameters.push_back(vkStructureCppParameter);
-            CppParameter vkAllocationCallbacksCppParameter { };
-            vkAllocationCallbacksCppParameter.cppDocumentation.brief = "A const pointer to a VkAllocationCallbacks structure to use while copying the given ${VK_STRUCTURE_TYPE}";
-            vkAllocationCallbacksCppParameter.cppDocumentation.notes.push_back("If pVkAllocationCallbacks is null any allocations will use new()");
-            vkAllocationCallbacksCppParameter.cppDocumentation.notes.push_back("If pVkAllocationCallbacks is not null the call to ${DESTROY_FUNCTION_NAME}() must provide a compatible VkAllocationCallbacks structure");
-            vkAllocationCallbacksCppParameter.type = "const VkAllocationCallbacks*";
-            vkAllocationCallbacksCppParameter.name = "pAllocationCallbacks";
-            vkAllocationCallbacksCppParameter.value = "nullptr";
-            cppFunction.cppParameters.push_back(vkAllocationCallbacksCppParameter);
+            CppParameter objCppParameter { };
+            objCppParameter.type = "const " + structure.name + "&";
+            objCppParameter.name = "obj";
+            cppFunction.cppParameters.push_back(objCppParameter);
+            CppParameter pAllocationCallbacksCppParameter { };
+            pAllocationCallbacksCppParameter.type = "const VkAllocationCallbacks*";
+            pAllocationCallbacksCppParameter.name = "pAllocationCallbacks";
+            pAllocationCallbacksCppParameter.value = "nullptr";
+            cppFunction.cppParameters.push_back(pAllocationCallbacksCppParameter);
             cppFunction.cppSourceBlock.replacements.push_back({ "${VK_STRUCTURE_TYPE}", structure.name });
             cppFunction.cppSourceBlock.add_snippet(R"(
                 ${VK_STRUCTURE_TYPE} result { };
@@ -90,172 +82,82 @@ public:
             )");
             cppFunctions.push_back(cppFunction);
         }
+        auto pNextHandler = generate_pnext_handler(xmlManifest);
         std::ofstream headerFile("create-structure-copy.hpp");
         if (headerFile.is_open()) {
             headerFile << cppFunctions.generate_declaration();
         }
         std::ofstream sourceFile("create-structure-copy.cpp");
         if (sourceFile.is_open()) {
-            sourceFile << cppFunctions.generate_definition();
+            for (auto& cppFunction : cppFunctions) {
+                if (structure_requires_custom_handling(cppFunction)) {
+                    cppFunction.cppCompileGuards.insert("DYNAMIC_STATIC_VK_STRUCTURE_REQUIRES_CUSTOM_HANDLING");
+                }
+            }
+            sourceFile << pNextHandler.generate_declaration() << std::endl;
+            sourceFile << cppFunctions.generate_definition() << std::endl;
+            sourceFile << pNextHandler.generate_definition();
         }
     }
 
 private:
     inline std::string generate_member_copy_snippet(
         const vk::xml::Manifest& vkXmlManifest,
-        const vk::xml::Parameter& vkStructureMember
+        const vk::xml::Parameter& vkXmlStructureMember
     )
     {
-        if (vkStructureMember.name == "pNext") {
+        if (vkXmlStructureMember.name == "pNext") {
             return "result.pNext = create_pnext_copy(obj.pNext, pAllocationCallbacks)";
         } else
-        if (vkStructureMember.flags & xml::Parameter::Array) {
-            if (vkStructureMember.flags & xml::Parameter::StringArray) {
-                return "// TODO : String Array : ${MEMBER_NAME}";
+        if (vkXmlStructureMember.flags & xml::Parameter::Array) {
+            if (vkXmlStructureMember.flags & xml::Parameter::StringArray) {
+                return "result.${MEMBER_NAME} = create_dynamic_string_array_copy(obj.${MEMBER_LENGTH}, obj.${MEMBER_NAME}, pAlloactionCallbacks);";
             } else 
-            if (vkStructureMember.flags & xml::Parameter::StaticArray) {
-                if (vkStructureMember.flags & xml::Parameter::String) {
-                    return "// TODO : Static String Array : ${MEMBER_NAME}";
+            if (vkXmlStructureMember.flags & xml::Parameter::StaticArray) {
+                if (vkXmlStructureMember.flags & xml::Parameter::String) {
+                    return "memcpy(result.${MEMBER_NAME}, obj.${MEMBER_NAME}, sizeof(obj.${MEMBER_NAME}));";
                 } else {
-                    return "// TODO : Static Array : ${MEMBER_NAME}";
+                    return "create_static_array_copy(result.${MEMBER_NAME}, obj.${MEMBER_NAME});";
                 }
             }
-            if (vkStructureMember.flags & xml::Parameter::DynamicArray) {
-                if (vkStructureMember.flags & xml::Parameter::String) {
-                    return "result.${MEMBER_NAME} = create_dynamic_string_array_copy(obj.${MEMBER_LENGTH}, obj.${MEMBER_NAME}, pAllocationCallbacks);";
+            if (vkXmlStructureMember.flags & xml::Parameter::DynamicArray) {
+                if (vkXmlStructureMember.flags & xml::Parameter::String) {
+                    return "result.${MEMBER_NAME} = create_dynamic_string_copy(obj.${MEMBER_NAME}, pAllocationCallbacks);";
                 } else {
                     return "result.${MEMBER_NAME} = create_dynamic_array_copy(obj.${MEMBER_LENGTH}, obj.${MEMBER_NAME}, pAllocationCallbacks);";
                 }
             }
         } else
-        if (vkStructureMember.flags & xml::Parameter::Pointer) {
-            if (vkStructureMember.flags & xml::Parameter::FunctionPointer) {
+        if (vkXmlStructureMember.flags & xml::Parameter::Pointer) {
+            if (vkXmlStructureMember.flags & xml::Parameter::FunctionPointer) {
                 return "result.${MEMBER_NAME} = obj.${MEMBER_NAME};";
             } else {
-                return "// TODO : Pointer : ${MEMBER_NAME}";
+                return "result.${MEMBER_NAME} = create_dynamic_array_copy(1, obj.${MEMBER_NAME}, pAllocationCallbacks);";
             }
         } else
-        if (vkXmlManifest.handles.find(vkStructureMember.type) != vkXmlManifest.handles.end()) {
+        if (vkXmlManifest.handles.find(vkXmlStructureMember.type) != vkXmlManifest.handles.end()) {
             return "result.${MEMBER_NAME} = obj.${MEMBER_NAME};";
         }
         return "result.${MEMBER_NAME} = create_structure_copy(obj.${MEMBER_NAME}, pAllocationCallbacks);";
     }
-};
 
-#if 0
-
-inline std::string strip_vk(const std::string& str)
-{
-    return string::remove(string::remove(string::remove(str, "VK_"), "Vk"), "vk");
-}
-
-/**
-TODO : Documentation
-*/
-class CreateStructureCopyGenerator final
-{
-public:
-    /**
-    TODO : Documentation
-    */
-    inline CreateStructureCopyGenerator(
-        const dst::vk::xml::Manifest& vkXmlManifest,
-        const std::string& includeDirectory
-    )
+    inline dst::cppgen::CppFunction generate_pnext_handler(const vk::xml::Manifest& vkXmlManifest)
     {
         using namespace dst::cppgen;
-        module.name = "create-structure-copy";
-        module.documentation.brief = "TODO : Documentation";
-        module.headerIncludes.insert({ Include::Type::External, "dynamic_static/graphics/vulkan/defines.hpp" });
-        auto includeFilePath = includeDirectory + "/" + "create-structure-copy.hpp";
-        module.sourceIncludes.insert({ Include::Type::Project, includeFilePath });
-        Namespace dstGfxVkNamespace("dst::gfx::vk");
-        Function createStructureCopyBase;
-        createStructureCopyBase.documentation.brief = "TODO : Documentation";
-        createStructureCopyBase.documentation.notes.push_back("TODO : Documentation");
-        createStructureCopyBase.documentation.notes.push_back("TODO : Documentation");
-        dstGfxVkNamespace.functions.push_back(createStructureCopyBase);
-
-        for (const auto& structureItr : vkXmlManifest.structures) {
-            const auto& structure = structureItr.second;
-            if (structure.alias.empty()) {
-                dstGfxVkNamespace.functions.push_back({ });
-                auto& createStructureCopyFunction = dstGfxVkNamespace.functions.back();
-                // createStructureCopyFunction.documentation.brief = "Creates a deep copy of a given " + structure.name;
-                createStructureCopyFunction.returnDocumentation = "The created " + structure.name;
-                createStructureCopyFunction.documentation.notes.push_back("The created " + structure.name + " must be destroyed using destroy_structure_copy()");
-                createStructureCopyFunction.documentation.notes.push_back("create_structure_copy() should not be used directly, prefer Managed<" + structure.name + ">");
-                createStructureCopyFunction.compileGuard = structure.compileGuard;
-                createStructureCopyFunction.returnType = structure.name;
-                createStructureCopyFunction.name = "create_structure_copy";
-
-                Function::Parameter structureParameter;
-                structureParameter.documentation.brief = "A const reference to the " + structure.name + " to create a copy of";
-                structureParameter.type = "const " + structure.name + "&";
-                structureParameter.name = "obj";
-                createStructureCopyFunction.parameters.push_back(structureParameter);
-
-                Function::Parameter allocatorParameter;
-                allocatorParameter.documentation.brief = "A const pointer to a VkAllocationCallbacks used to control allocations";
-                allocatorParameter.type = "const VkAllocationCallbacks*";
-                allocatorParameter.name = "pAllocator";
-                allocatorParameter.defaultValue = "nullptr";
-                createStructureCopyFunction.parameters.push_back(allocatorParameter);
-
-                createStructureCopyFunction.body << structure.name << " result;" << std::endl;
-                for (const auto& member : structure.members) {
-                    if (member.name == "pNext") {
-                        // process_pnext_member();
-                    } else
-                    if (member.flags & xml::Parameter::Array) {
-                        if (member.flags & xml::Parameter::StringArray) {
-                            // process_string_array_member();
-                        } else 
-                        if (member.flags & xml::Parameter::StaticArray) {
-                            if (member.flags & xml::Parameter::String) {
-                                // process_statically_sized_string_member();
-                            } else {
-                                // process_statically_sized_array_member();
-                            }
-                        }
-                        if (member.flags & xml::Parameter::DynamicArray) {
-                            if (member.flags & xml::Parameter::String) {
-                                // process_dynamically_sized_string_member();
-                            } else {
-                                // process_dynamically_sized_array_member();
-                            }
-                        }
-                    } else
-                    if (member.flags & xml::Parameter::Pointer) {
-                        if (member.flags & xml::Parameter::FunctionPointer) {
-                            // process_function_pointer_member();
-                        } else {
-                            // process_pointer_member();
-                        }
-                    } else
-                    if (vkXmlManifest.handles.find(member.type) != vkXmlManifest.handles.end()) {
-                        // process_vk_handle_member();
-                    } else {
-                        process_member(member, createStructureCopyFunction.body);
-                    }
-                }
-                createStructureCopyFunction.body << "return result;" << std::endl;
-            }
-        }
-        module.namespaces.push_back(dstGfxVkNamespace);
+        CppFunction cppFunction { };
+        cppFunction.name = "create_pnext_copy";
+        CppParameter pNextCppParameter { };
+        pNextCppParameter.type = "void*";
+        pNextCppParameter.name = "pNext";
+        cppFunction.cppParameters.push_back(pNextCppParameter);
+        CppParameter pAllocationCallbacksCppParameter { };
+        pAllocationCallbacksCppParameter.type = "const VkAllocationCallbacks*";
+        pAllocationCallbacksCppParameter.name = "pAllocationCallbacks";
+        cppFunction.cppParameters.push_back(pAllocationCallbacksCppParameter);
+        cppFunction.cppSourceBlock.add_snippet("return nullptr;");
+        return cppFunction;
     }
-
-    ///////////////////////////////////////////////////////////////////////////////
-    inline void process_member(const xml::Parameter& member, std::stringstream& strStrm)
-    {
-        strStrm << "result." + member.name + " = obj." + member.name + ";" << std::endl;
-    }
-    ///////////////////////////////////////////////////////////////////////////////
-
-    dst::cppgen::Module module; //!< TODO : Documentation
 };
-
-#endif
 
 } // namespace cppgen
 } // namespace vk
--- include.ex/dynamic_static/graphics/vulkan/create-structure-copy-utilities.hpp
+++ include.ex/dynamic_static/graphics/vulkan/create-structure-copy-utilities.hpp
@@ -15,26 +15,126 @@
 namespace dst {
 namespace gfx {
 namespace vk {
+namespace detail {
+
+/**
+Creates a deep copy of a given VulkanStructureType
+@param <VulkanStructureType> The type of Vulkan structure to create a deep copy of
+@param [in] obj A const reference to the VulkanStructureType to create a deep copy of
+@param [in](optional = nullptr) pAllocationCallbacks A const pointer to a VkAllocationCallbacks structure to use while copying the given VulkanStructureType
+@return The deep copy of the given VulkanStructureType
+    @note Any VulkanStructureType copied via create_structure_copy<>() must be destroyed via destroy_structure_copy<>()
+    @note Managed<VulkanStructureType> should be preferred over direct useage of create_structure_copy<>()/destroy_structure_copy<>()
+    @note If pAllocationCallbacks is null any allocations will use malloc()
+    @note If pAllocationCallbacks is not null the call to destroy_structure_copy<>() must provide a compatible VkAllocationCallbacks structure
+*/
+template <typename VulkanStructureType>
+inline VulkanStructureType create_structure_copy(
+    const VulkanStructureType& obj,
+    const VkAllocationCallbacks* pAllocationCallbacks = nullptr
+)
+{
+    (void)pAllocationCallbacks;
+    return obj;
+}
 
 /**
 TODO : Documentation
 */
-template <typename VkStructureType>
-VkStructureType create_structure_copy(const VkStructureType& obj)
+template <typename VulkanStructureType>
+inline VulkanStructureType destroy_structure_copy(
+    VulkanStructureType& obj,
+    const VkAllocationCallbacks* pAllocationCallbacks = nullptr
+)
 {
-    return obj;
+    (void)obj;
+    (void)pAllocationCallbacks;
 }
 
-namespace detail {
-
-inline void create_dynamic_string_array_copy()
+/**
+TODO : Documentation
+*/
+inline const VkAllocationCallbacks* validate_allocation_callbacks(const VkAllocationCallbacks* pAllocationCallbacks)
 {
-
+    static const VkAllocationCallbacks sAllocationCallbacks {
+        nullptr,
+        [](void*, size_t size, size_t, VkSystemAllocationScope) { return malloc(size); },
+        nullptr,
+        [](void*, void* pMemory) { free(pMemory); },
+        nullptr,
+        nullptr,
+    };
+    return pAllocationCallbacks ? pAllocationCallbacks : &sAllocationCallbacks;
 }
 
-inline void create_dynamic_array_copy()
+/**
+TODO : Documentation
+*/
+inline char* create_dynamic_string_copy(
+    const char* pStr,
+    const VkAllocationCallbacks* pAllocationCallbacks
+)
 {
+    char* pResult = nullptr;
+    if (pStr) {
+        pAllocationCallbacks = validate_allocation_callbacks(pAllocationCallbacks);
+        auto count = strlen(pStr) + 1;
+        pResult = (char*)pAllocationCallbacks->pfnAllocation(nullptr, sizeof(char) * count, 0, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+        memcpy(pResult, pStr, count);
+    }
+    return pResult;
+}
 
+/**
+TODO : Documentation
+*/
+inline char** create_dynamic_string_array_copy(
+    uint32_t count,
+    const char* const* ppStrs,
+    const VkAllocationCallbacks* pAllocationCallbacks
+)
+{
+    char** ppResult = nullptr;
+    if (count && ppStrs) {
+        pAllocationCallbacks = validate_allocation_callbacks(pAllocationCallbacks);
+        ppResult = (char**)pAllocationCallbacks->pfnAllocation(nullptr, sizeof(char*) * count, 0, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+        for (uint32_t i = 0; i < count; ++i) {
+            ppResult[i] = create_dynamic_string_copy(ppStrs[i], pAllocationCallbacks);
+        }
+    }
+    return ppResult;
+}
+
+/**
+TODO : Documentation
+*/
+template <typename VulkanStructureType, size_t Count>
+inline void create_static_array_copy(VulkanStructureType pDst[Count], const VulkanStructureType pSrc[Count])
+{
+    for (size_t i = 0; i < Count; ++i) {
+        pDst[i] = create_structure_copy(pSrc[i]);
+    }
+}
+
+/**
+TODO : Documentation
+*/
+template <typename VulkanStructureType>
+inline VulkanStructureType* create_dynamic_array_copy(
+    uint32_t count,
+    const VulkanStructureType* pObjs,
+    const VkAllocationCallbacks* pAllocationCallbacks
+)
+{
+    VulkanStructureType* pResult = nullptr;
+    if (count && pObjs) {
+        pAllocationCallbacks = validate_allocation_callbacks(pAllocationCallbacks);
+        pResult = (VulkanStructureType*)pAllocationCallbacks->pfnAllocation(nullptr, sizeof(VulkanStructureType) * count, 0, VK_SYSTEM_ALLOCATION_SCOPE_OBJECT);
+        for (uint32_t i = 0; i < count; ++i) {
+            pResult[i] = create_structure_copy(pObjs[i], pAllocationCallbacks);
+        }
+    }
+    return pResult;
 }
 
 } // namespace detail
