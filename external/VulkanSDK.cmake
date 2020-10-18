4cd9374335d26b395565d676d16a186a70483841
Patrick Purcell <patrick.purcell@intel.com>
2020-10-17 18:59:12 -0700

    Removing submodules

--- external/VulkanSDK.cmake
+++
@@ -1,7 +0,0 @@
-
-set(VulkanSDK.includeDirectories "${PROJECT_SOURCE_DIR}/external/VulkanSDK/Include/")
-if(MSVC)
-    file(TO_CMAKE_PATH "$ENV{VULKAN_SDK}/Lib/vulkan-1.lib" VulkanSDK.linkLibraries)
-else()
-    set(VulkanSDK.linkLibraries "/usr/lib/aarch64-linux-gnu/libvulkan.so.1")
-endif()
