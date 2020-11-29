d6d1eba33bb0bb4b4e4637c695cf40ae4c1e545f
Patrick Purcell <patrick.purcell@intel.com>
2020-11-28 21:27:58 -0800

    General cleanup

--- external/SPIRV-Cross.cmake
+++ external/SPIRV-Cross.cmake
@@ -1,24 +1,41 @@
 
-#### dst_add_external_project(
-####     target SPIRV-Cross
-####     URL https://github.com/KhronosGroup/SPIRV-Cross/archive/master.zip
-#### )
-#### 
-#### set(SPIRV-Cross.includeDirectories "${SPIRV-Cross.sourceDirectory}/")
-#### if (MSVC)
-####     set(SPIRV-Cross.linkLibraries
-####         "${SPIRV-Cross.buildDirectory}/$(Configuration)/spirv-cross-core.lib"
-####         "${SPIRV-Cross.buildDirectory}/$(Configuration)/spirv-cross-cpp.lib"
-####         "${SPIRV-Cross.buildDirectory}/$(Configuration)/spirv-cross-glsl.lib"
-####         "${SPIRV-Cross.buildDirectory}/$(Configuration)/spirv-cross-hlsl.lib"
-####         "${SPIRV-Cross.buildDirectory}/$(Configuration)/spirv-cross-msl.lib"
-####     )
-#### else()
-####     set(SPIRV-Cross.linkLibraries
-####         "${SPIRV-Cross.buildDirectory}/libspirv-cross-core.a"
-####         "${SPIRV-Cross.buildDirectory}/libspirv-cross-cpp.a"
-####         "${SPIRV-Cross.buildDirectory}/libspirv-cross-glsl.a"
-####         "${SPIRV-Cross.buildDirectory}/libspirv-cross-hlsl.a"
-####         "${SPIRV-Cross.buildDirectory}/libspirv-cross-msl.a"
-####     )
-#### endif()
+# ==========================================
+#   Copyright (c) 2020 dynamic_static
+#     Patrick Purcell
+#       Licensed under the MIT license
+#     http://opensource.org/licenses/MIT
+# ==========================================
+
+include(FetchContent)
+set(SPIRV_CROSS_CLI          OFF CACHE BOOL "" FORCE)
+set(SPIRV_CROSS_ENABLE_TESTS OFF CACHE BOOL "" FORCE)
+set(SPIRV_CROSS_FORCE_PIC    ON  CACHE BOOL "" FORCE)
+set(SPIRV_CROSS_SKIP_INSTALL ON  CACHE BOOL "" FORCE)
+FetchContent_Declare(
+    SPIRV-Cross GIT_REPOSITORY
+    "https://github.com/KhronosGroup/SPIRV-Cross.git"
+    GIT_TAG 2020-09-17
+    GIT_PROGRESS TRUE
+    FETCHCONTENT_UPDATES_DISCONNECTED
+)
+FetchContent_MakeAvailable(SPIRV-Cross)
+set_target_properties(spirv-cross-c       PROPERTIES FOLDER "external/SPIRV-Cross/")
+set_target_properties(spirv-cross-core    PROPERTIES FOLDER "external/SPIRV-Cross/")
+set_target_properties(spirv-cross-cpp     PROPERTIES FOLDER "external/SPIRV-Cross/")
+set_target_properties(spirv-cross-glsl    PROPERTIES FOLDER "external/SPIRV-Cross/")
+set_target_properties(spirv-cross-hlsl    PROPERTIES FOLDER "external/SPIRV-Cross/")
+set_target_properties(spirv-cross-msl     PROPERTIES FOLDER "external/SPIRV-Cross/")
+set_target_properties(spirv-cross-reflect PROPERTIES FOLDER "external/SPIRV-Cross/")
+set_target_properties(spirv-cross-util    PROPERTIES FOLDER "external/SPIRV-Cross/")
+add_library(SPIRV-Cross_ INTERFACE)
+target_link_libraries(
+    SPIRV-Cross_ INTERFACE
+    spirv-cross-c
+    spirv-cross-core
+    spirv-cross-cpp
+    spirv-cross-glsl
+    spirv-cross-hlsl
+    spirv-cross-msl
+    spirv-cross-reflect
+    spirv-cross-util
+)
--- external/glslang.cmake
+++ external/glslang.cmake
@@ -6,55 +6,10 @@
 #     http://opensource.org/licenses/MIT
 # ==========================================
 
-#file(REMOVE_RECURSE "${glslang.sourceDirectory}/External")
-#file(REMOVE_RECURSE "${glslang.sourceDirectory}/gtests")
-#file(REMOVE_RECURSE "${glslang.sourceDirectory}/StandAlone")
-#file(REMOVE_RECURSE "${glslang.sourceDirectory}/Test")
-#set(glslang.includeDirectories "${glslang.sourceDirectory}/")
-#if (MSVC)
-#    set(glslang.linkLibraries
-#        "${glslang.buildDirectory}/glslang/$(Configuration)/glslang*.lib"
-#        "${glslang.buildDirectory}/glslang/OSDependent/Windows/$(Configuration)/OSDependent*.lib"
-#        "${glslang.buildDirectory}/hlsl/$(Configuration)/HLSL*.lib"
-#        "${glslang.buildDirectory}/OGLCompilersDLL/$(Configuration)/OGLCompiler*.lib"
-#        "${glslang.buildDirectory}/SPIRV/$(Configuration)/SPIRV*.lib"
-#        "${glslang.buildDirectory}/SPIRV/$(Configuration)/SPVRemapper*.lib"
-#    )
-#else()
-#    set(glslang.linkLibraries
-#        "${glslang.buildDirectory}/glslang/libglslang.a"
-#        "${glslang.buildDirectory}/glslang/OSDependent/Unix/libOSDependent.a"
-#        "${glslang.buildDirectory}/hlsl/libHLSL.a"
-#        "${glslang.buildDirectory}/OGLCompilersDLL/libOGLCompiler.a"
-#        "${glslang.buildDirectory}/SPIRV/libSPIRV.a"
-#        "${glslang.buildDirectory}/SPIRV/libSPVRemapper.a"
-#    )
-#endif()
-
-#### set(glslang_sourceDirectory "${externalDirectory}/glslang/")
-#### set(glslang_buildDirectory "${CMAKE_BINARY_DIR}/external/glslang/")
-#### dst_add_external_cmake_project(
-####     project glslang
-####     sourceDirectory "${glslang_sourceDirectory}"
-####     buildDirectory "${glslang_buildDirectory}"
-#### )
-#### target_include_directories(glslang INTERFACE "${externalDirectory}/")
-#### target_link_libraries(
-####     glslang INTERFACE
-####     "${glslang_buildDirectory}/glslang/$(Configuration)/GenericCodeGen*.lib"
-####     "${glslang_buildDirectory}/glslang/$(Configuration)/glslang*.lib"
-####     "${glslang_buildDirectory}/glslang/$(Configuration)/MachineIndependent*.lib"
-####     "${glslang_buildDirectory}/glslang/OSDependent/Windows/$(Configuration)/OSDependent*.lib"
-####     "${glslang_buildDirectory}/hlsl/$(Configuration)/HLSL*.lib"
-####     "${glslang_buildDirectory}/OGLCompilersDLL/$(Configuration)/OGLCompiler*.lib"
-####     "${glslang_buildDirectory}/SPIRV/$(Configuration)/SPIRV*.lib"
-####     "${glslang_buildDirectory}/SPIRV/$(Configuration)/SPVRemapper*.lib"
-#### )
-
 include(FetchContent)
 set(ENABLE_GLSLANG_INSTALL OFF CACHE BOOL "" FORCE)
-set(ENABLE_GLSLANG_JS OFF CACHE BOOL "" FORCE)
-set(ENABLE_GLSLANG_WEBMIN OFF CACHE BOOL "" FORCE)
+set(ENABLE_GLSLANG_JS      OFF CACHE BOOL "" FORCE)
+set(ENABLE_GLSLANG_WEBMIN  OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
     glslang GIT_REPOSITORY
     "https://github.com/KhronosGroup/glslang.git"
